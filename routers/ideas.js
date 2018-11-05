73a4312ec0afd7e0fd350a52b0166f0031397505
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-05 17:01:08 +0530

    refactoring code create folder routes create onther file ideas.js place all router of ideas CRUD

---
+++ routers/ideas.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+// Load Idea Model
+require('../models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Idea Index Page
+router.get('/', (req, res) => {
+  Idea.find({})
+    .sort({
+      date: 'desc'
+    })
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas: ideas
+      });
+    });
+});
+
+// Add Idea Form
+router.get('/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+//edit  Idea Form
+// router.get('/ideas/edit/:id', (req, res) => {
+//   Idea.findOne({
+//     _id: req.param.id
+//   }).then(idea => {
+//     res.render('ideas/edit', {
+//       idea: idea
+//     });
+//   });
+// });
+
+// Edit Idea Form
+router.get('/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render('ideas/edit', {
+      idea: idea
+    });
+  });
+});
+
+// Process Form
+router.post('/', (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({
+      text: 'Please add a title'
+    });
+  }
+  if (!req.body.details) {
+    errors.push({
+      text: 'Please add some details'
+    });
+  }
+
+  if (errors.length > 0) {
+    res.render('/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser).save().then(idea => {
+      req.flash('success_msg', 'Content Added Successfully ');
+      res.redirect('/ideas');
+    });
+  }
+});
+
+//update form frocess
+
+router.put('/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+    idea.save().then(idea => {
+      req.flash('success_msg', 'Content Update Successfully ');
+      res.redirect('/ideas');
+    });
+  });
+});
+
+//delete ideas
+
+router.delete('/:id', (req, res) => {
+  Idea.deleteOne({
+    _id: req.params.id
+  }).then(() => {
+    req.flash('success_msg', 'Content Delete Successfully ');
+    res.redirect('/ideas');
+  });
+});
+
+module.exports = router;
