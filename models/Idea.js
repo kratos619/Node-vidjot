ad01263d256d7961e42b36b8d73424c6b2f4e465
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-05 22:56:04 +0530

    add user to schema to grab user id

--- models/Idea.js
+++ models/Idea.js
@@ -3,11 +3,15 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const IdeaSchema = new Schema({
-  title:{
+  title: {
     type: String,
     required: true
   },
-  details:{
+  details: {
+    type: String,
+    required: true
+  },
+  user: {
     type: String,
     required: true
   },
