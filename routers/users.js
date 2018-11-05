f12458a6f1b3e8fd53fb25c1917eca6dd5ca276b
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-05 20:10:23 +0530

    add register user validations

--- routers/users.js
+++ routers/users.js
@@ -12,4 +12,38 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
+// user post routers
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if (req.body.password != req.body.password2) {
+    errors.push({
+      text: 'password Do Not Match..'
+    });
+  }
+
+  if (req.body.password.length < 4) {
+    errors.push({
+      text: 'Password Lenght Shuld be More than 4 '
+    });
+  }
+
+  if (!req.body.password) {
+    errors.push({ text: 'Password Can Not Be Blank ' });
+  }
+  if (!req.body.password2) {
+    errors.push({ text: 'Confirm Password Can Not Be Blank ' });
+  }
+
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email
+    });
+  } else {
+    res.send('passed');
+  }
+});
+
 module.exports = router;
