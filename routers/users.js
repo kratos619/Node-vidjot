db02d63376f1ef67acadeb8d5e23ece4dfc71fc8
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-05 19:33:46 +0530

    create users route

--- routers/users.js
+++ routers/users.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+// userlogin
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+// user Register
+router.get('/register', (req, res) => {
+  res.render('users/register');
+});
+
 module.exports = router;
